a69e82ad7fa32a5604db3708a06c9eb2e7cbf0fb
baldoghi <baldoghi@inf.u-szeged.hu>
2018-05-04 13:53:41 +0200

    calc value, alert

--- src/components/calculator.ts
+++ src/components/calculator.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AlertController } from 'ionic-angular';
 
 @Component({
     selector: 'calculator',
@@ -9,13 +10,23 @@ export class Calculator {
     public height: number;
     public weight: number;
 
-    constructor() {
-
+    constructor(public alertCtrl: AlertController) {
     }
 
-    private onSubmit(form) {
-
+    private onSubmit(form): void {
+        if(form.valid) {
+            let alert = this.alertCtrl.create({
+                title: 'BMI',
+                subTitle: this.bmiCalculator().toString(),
+                buttons: ['OK']
+            })
+            alert.present();
+        }
     }
 
-
+    private bmiCalculator(): number {
+        //if(typeof this.height == "number") {
+            return this.weight / Math.pow(this.height, 2) * 10000;
+        //}
+    }
 }

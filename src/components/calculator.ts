169216f9fe78aafda11c3aa0f2f10111260ba2cf
baldoghi <baldoghi@inf.u-szeged.hu>
2018-05-04 13:12:05 +0200

    add calculator form

---
+++ src/components/calculator.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'calculator',
+    templateUrl: 'calculator.html'
+})
+
+export class Calculator {
+    constructor() {
+
+    }
+}
